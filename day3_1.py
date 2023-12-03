90afb64e588cb7ff6ac770518a1a6e9b20019342
Katerina Shananina <serpent.k@gmail.com>
2023-12-03 18:41:27 +0300

    Add day 3

---
+++ day3_1.py
@@ -0,0 +1,30 @@
+# usage: python3 day3_2.py input3.txt
+
+import re
+import sys
+
+def is_symbol(c):
+    return (c != '.' and not c.isdigit())
+
+def line_sum(prevline, line, nextline):
+    res = 0
+    for match in re.finditer(r'\d+', line):
+        start = max(match.start() - 1, 0)
+        end = min(140, match.end() + 1)
+        if any(map(is_symbol,
+                   prevline[start : end] +
+                   line[start : end] +
+                   nextline[start : end])):
+            res += int(match.group())
+    return res
+
+f = open(sys.argv[1])
+emptyline = '.' * 140
+prevline = f.readline()
+line = f.readline()
+s = line_sum(emptyline, prevline, line)
+for nextline in f:
+    s += line_sum(prevline, line, nextline)
+    prevline, line = line, nextline
+s += line_sum(prevline, line, emptyline)
+print(s)
---
+++ day3_2.py
@@ -0,0 +1,29 @@
+# usage: python3 day3_2.py input3.txt
+
+import re
+import sys
+
+
+def line_sum(prevline, line, nextline):
+    res = 0
+    prev_nums = list(re.finditer(r'\d+', prevline))
+    line_nums = list(re.finditer(r'\d+', line))
+    next_nums = list(re.finditer(r'\d+', nextline))
+    for match in re.finditer(r'\*', line):
+        matches = [m for m in prev_nums + line_nums + next_nums
+                   if (m.start() < match.start() + 2 and
+                       m.end() > match.start() - 1)]
+        if len(matches) == 2:
+            res += int(matches[0].group()) * int(matches[1].group())      
+    return res
+
+f = open(sys.argv[1])
+emptyline = '.' * 140
+prevline = f.readline()
+line = f.readline()
+s = line_sum(emptyline, prevline, line)
+for nextline in f:
+    s += line_sum(prevline, line, nextline)
+    prevline, line = line, nextline
+s += line_sum(prevline, line, emptyline)
+print(s)
