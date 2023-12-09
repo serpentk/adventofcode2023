a4f9dbd9f9a11df60dae2b527135212226e39047
Katerina N. Shananina <eshananina@rambler-co.ru>
2023-12-09 15:25:09 +0300

    Day 9

---
+++ day9_1.py
@@ -0,0 +1,13 @@
+import sys
+
+def extrapolate(numbers):
+    if all([x == 0 for x in numbers]):
+        return 0
+    return numbers[-1] + extrapolate(
+        [numbers[i + 1] - numbers[i] for i in range(len(numbers) - 1)])
+
+s = 0
+for line in sys.stdin:
+    s += extrapolate([int(x) for x in line.split()])
+
+print(s)
