bc8d921ce081cba33cd45d85155e9747999fd132
Katerina N. Shananina <eshananina@rambler-co.ru>
2023-12-13 00:21:59 +0300

    Day 12, part 2

--- day12_2.py
+++ day12_2.py
@@ -1,31 +1,46 @@
 import sys
 import re
 
-
-def get_re(springs):
-    replace_map = {'?': '.', '.': '\.', '#': '#'}
-    return re.compile('^' + ''.join(replace_map[c] for c in springs) + '$')
-
 def get_springs(valid, broken):
-    return ''.join('.' * v + '#' * b for (v,b) in zip(valid, broken)) + '.' * v[-1]
+    return ''.join('.' * v + '#' * b for (v,b) in zip(valid, broken)) + '.' * valid[-1]
 
-def versions(n, spaces, first):
+def match_c(c0, c):
+    if c0 == '?':
+        return True
+    return c == c0
+
+def match(template, springs):
+    return all([match_c(c, springs[i]) for i, c in enumerate(template)])
+
+
+cache = {}
+            
+def versions_count(n, spaces, first, broken, template):
+    #print(n, spaces, first, broken, template)
+    vc = cache.get((n, spaces, first, tuple(broken), template))
+    if vc is not None:
+        return vc
+    
     if spaces == 1:
-        yield [n]
-    elif n >= spaces - 1:
+        if (n == 0 or match(template[-n:], '.' * n)):
+            vc = 1
+        else:
+            vc = 0
+    elif n >= spaces - (2 if first else 1):
+        vc = 0
         for x in range(0 if first else 1, n + 1):
-            for v in versions(n - x, spaces - 1, False):
-                yield [x] + v
-
+            if (match(template[:x + broken[0]], '.' * x + '#' * broken[0])):
+                vc += versions_count(n - x, spaces - 1, False, broken[1:], template[(x + broken[0]):])
+    else:
+        vc = 0
+    cache[(n, spaces, first, tuple(broken), template)] = vc
+    return vc
+    
+            
 s = 0
 for line in sys.stdin:
-    print(line.strip())
     springs, control = line.strip().split()
     control = list(map(int, control.split(',') * 5))
     springs = '?'.join([springs] * 5)
-    r = get_re(springs)
-    for v in versions(sum(control), len(control) + 1, True):
-        # print(get_springs(v, control))
-        if r.match(get_springs(v, control)):
-            s += 1
+    s += versions_count(len(springs) - sum(control), len(control) + 1, True, control, springs)
 print(s)
