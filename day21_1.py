e7eb3827c1a1877ee4077868829ee1aa9579c91a
Katerina N. Shananina <eshananina@rambler-co.ru>
2023-12-21 08:59:14 +0300

    Day 21, part 1

---
+++ day21_1.py
@@ -0,0 +1,22 @@
+import sys
+
+STEPS = 6
+garden = []
+actual = set()
+for i, line in enumerate(sys.stdin):
+    garden.append(line.strip())
+    j = line.find('S')
+    if j > -1:
+        actual.add((j, i))
+
+for step in range(STEPS):
+    new_reached = set()
+    for (xp, yp) in actual:
+        nbrs = {(x, y)
+                for x, y in ((xp + 1, yp), (xp - 1, yp), (xp, yp + 1), (xp, yp - 1))
+                if x >=0 and y >=0 and x < len(garden[0]) and y < len(garden) and garden[y][x] != '#'}
+        new_reached.update(nbrs)
+    actual = new_reached
+        
+print(len(actual))
+
